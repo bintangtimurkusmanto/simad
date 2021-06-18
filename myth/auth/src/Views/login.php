5555bf551d9ba08c0341b0379b32a87c701a7cca
bintangtimurk <b.timur504@gmail.com>
2021-06-18 15:57:20 +0700

    view login dan register

--- myth/auth/src/Views/login.php
+++ myth/auth/src/Views/login.php
@@ -1,72 +1,82 @@
-<?= $this->extend($config->viewLayout) ?>
-<?= $this->section('main') ?>
+<?= $this->extend('auth/template/index'); ?>
 
-<div class="container">
-	<div class="row">
-		<div class="col-sm-6 offset-sm-3">
+<?= $this->section('title'); ?>
+Login &mdash; SIMAD
+<?= $this->endSection(); ?>
 
-			<div class="card">
-				<h2 class="card-header"><?= lang('Auth.loginTitle') ?></h2>
-				<div class="card-body">
+<?= $this->section('content'); ?>
+<div id="app">
+    <section class="section">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    <div class="login-brand">
+                        <img src="<?= base_url(); ?>/stisla/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                    </div>
 
-					<?= view('Myth\Auth\Views\_message_block') ?>
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4 class="card-header"><?= lang('Auth.loginTitle') ?></h4>
+                        </div>
 
-					<form action="<?= route_to('login') ?>" method="post">
-						<?= csrf_field() ?>
 
-						<?php if ($config->validFields === ['email']) : ?>
-							<div class="form-group">
-								<label for="login"><?= lang('Auth.email') ?></label>
-								<input type="email" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.email') ?>">
-								<div class="invalid-feedback">
-									<?= session('errors.login') ?>
-								</div>
-							</div>
-						<?php else : ?>
-							<div class="form-group">
-								<label for="login"><?= lang('Auth.emailOrUsername') ?></label>
-								<input type="text" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>">
-								<div class="invalid-feedback">
-									<?= session('errors.login') ?>
-								</div>
-							</div>
-						<?php endif; ?>
+                        <div class="card-body">
 
-						<div class="form-group">
-							<label for="password"><?= lang('Auth.password') ?></label>
-							<input type="password" name="password" class="form-control  <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>">
-							<div class="invalid-feedback">
-								<?= session('errors.password') ?>
-							</div>
-						</div>
+                            <?= view('Myth\Auth\Views\_message_block') ?>
 
-						<?php if ($config->allowRemembering) : ?>
-							<div class="form-check">
-								<label class="form-check-label">
-									<input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked <?php endif ?>>
-									<?= lang('Auth.rememberMe') ?>
-								</label>
-							</div>
-						<?php endif; ?>
+                            <form action="<?= route_to('login') ?>" class="needs-validation" method="post">
+                                <?= csrf_field() ?>
 
-						<br>
+                                <?php if ($config->validFields === ['email']) : ?>
+                                    <div class="form-group">
+                                        <label for="login"><?= lang('Auth.email') ?></label>
+                                        <input id="email" type="email" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.email') ?>" tabindex="1" required>
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.login') ?>
+                                        </div>
+                                    </div>
+                                <?php else : ?>
+                                    <div class="form-group">
+                                        <label for="login"><?= lang('Auth.emailOrUsername') ?></label>
+                                        <input id="email" type="text" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>" tabindex="1" required>
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.login') ?>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
 
-						<button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.loginAction') ?></button>
-					</form>
+                                <div class="form-group">
+                                    <div class="d-block">
+                                        <label for="password"><?= lang('Auth.password') ?></label>
+                                    </div>
+                                    <input id="password" name="password" type="password" class="form-control<?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>" tabindex="2" required>
+                                    <div class="invalid-feedback">
+                                        <?= session('errors.password') ?>
+                                    </div>
+                                </div>
 
-					<hr>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        <?= lang('Auth.loginAction') ?>
+                                    </button>
+                                </div>
+                            </form>
 
-					<?php if ($config->allowRegistration) : ?>
-						<p><a href="<?= route_to('register') ?>"><?= lang('Auth.needAnAccount') ?></a></p>
-					<?php endif; ?>
-					<?php if ($config->activeResetter) : ?>
-						<p><a href="<?= route_to('forgot') ?>"><?= lang('Auth.forgotYourPassword') ?></a></p>
-					<?php endif; ?>
-				</div>
-			</div>
-
-		</div>
-	</div>
+                        </div>
+                    </div>
+                    <div class="mt-5 text-muted text-center">
+                        <?php if ($config->allowRegistration) : ?>
+                            <a href="<?= route_to('register') ?>"><?= lang('Auth.needAnAccount') ?></a>
+                        <?php endif; ?>
+                        <br>
+                    </div>
+                    <div class="simple-footer">
+                        Copyright &copy; SIMAD <?= date('Y'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 
-<?= $this->endSection() ?>
+<?= $this->endSection(); ?>
--- myth/auth/src/Views/register.php
+++ myth/auth/src/Views/register.php
@@ -1,55 +1,123 @@
-<?= $this->extend($config->viewLayout) ?>
-<?= $this->section('main') ?>
+<?= $this->extend('auth/template/index'); ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-6 offset-sm-3">
+<?= $this->section('title'); ?>
+Register &mdash; SIMAD
+<?= $this->endSection(); ?>
 
-            <div class="card">
-                <h2 class="card-header"><?=lang('Auth.register')?></h2>
-                <div class="card-body">
 
-                    <?= view('Myth\Auth\Views\_message_block') ?>
+<?= $this->section('content'); ?>
+<div id="app">
+    <section class="section">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                    <div class="login-brand">
+                        <img src="<?= base_url(); ?>/stisla/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                    </div>
 
-                    <form action="<?= route_to('register') ?>" method="post">
-                        <?= csrf_field() ?>
-
-                        <div class="form-group">
-                            <label for="email"><?=lang('Auth.email')?></label>
-                            <input type="email" class="form-control <?php if(session('errors.email')) : ?>is-invalid<?php endif ?>"
-                                   name="email" aria-describedby="emailHelp" placeholder="<?=lang('Auth.email')?>" value="<?= old('email') ?>">
-                            <small id="emailHelp" class="form-text text-muted"><?=lang('Auth.weNeverShare')?></small>
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4><?= lang('Auth.register') ?></h4>
                         </div>
 
-                        <div class="form-group">
-                            <label for="username"><?=lang('Auth.username')?></label>
-                            <input type="text" class="form-control <?php if(session('errors.username')) : ?>is-invalid<?php endif ?>" name="username" placeholder="<?=lang('Auth.username')?>" value="<?= old('username') ?>">
+                        <div class="card-body">
+
+                            <form action="<?= route_to('register') ?>" method="post">
+                                <?= csrf_field() ?>
+
+
+                                <div class="form-group">
+                                    <label for="nama">Nama Lengkap</label>
+                                    <input id="nama" type="text" class="form-control <?php if (session('errors.nama')) : ?>is-invalid<?php endif ?>" name="nama" placeholder="Nama lengkap" value="<?= old('nama') ?>">
+                                    <div class="invalid-feedback">
+                                        <?= session('errors.nama'); ?>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-6">
+                                        <label for="email"><?= lang('Auth.email') ?></label>
+                                        <input id="email" type="email" class="form-control <?php if (session('errors.email')) : ?>is-invalid<?php endif ?>" name="email" value="<?= old('email') ?>" placeholder="<?= lang('Auth.email') ?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.email'); ?>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group col-6">
+                                        <label for="username"><?= lang('Auth.username') ?></label>
+                                        <input id="username" type="text" class="form-control <?php if (session('errors.username')) : ?>is-invalid<?php endif ?>" name="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.username'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-6">
+                                        <label for="nim">NIM / NIP</label>
+                                        <input id="nim" type="text" class="form-control <?php if (session('errors.nim')) : ?>is-invalid<?php endif ?>" name="nim" placeholder="NIM atau NIP" value="<?= old('nim') ?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.nim'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="no_hp">No. HP</label>
+                                        <input id="no_hp" type="number" class="form-control <?php if (session('errors.no_hp')) : ?>is-invalid<?php endif ?>" name="no_hp" placeholder="Nomor HP aktif" value="<?= old('no_hp') ?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.no_hp'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="alamat">Alamat</label>
+                                    <input id="alamat" type="text" class="form-control <?php if (session('errors.alamat')) : ?>is-invalid<?php endif ?>" name="alamat" placeholder="Alamat" value="<?= old('alamat') ?>">
+                                    <div class="invalid-feedback">
+                                        <?= session('errors.alamat'); ?>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="row">
+                                    <div class="form-group col-6">
+                                        <label for="password"><?= lang('Auth.password') ?></label>
+                                        <input id="password" type="password" class="form-control pwstrength <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" data-indicator="pwindicator" name="password" placeholder="<?= lang('Auth.password') ?>" autocomplete="off">
+                                        <div id="pwindicator" class="pwindicator">
+                                            <div class="bar"></div>
+                                            <div class="label"></div>
+                                        </div>
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.password'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="pass_confirm"><?= lang('Auth.repeatPassword') ?></label>
+                                        <input id="password2" type="password" class="form-control <?php if (session('errors.pass_confirm')) : ?>is-invalid<?php endif ?>" name="pass_confirm" placeholder="<?= lang('Auth.repeatPassword') ?>" autocomplete="off">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.pass_confirm'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block">
+                                        <?= lang('Auth.register') ?>
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password"><?=lang('Auth.password')?></label>
-                            <input type="password" name="password" class="form-control <?php if(session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?=lang('Auth.password')?>" autocomplete="off">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="pass_confirm"><?=lang('Auth.repeatPassword')?></label>
-                            <input type="password" name="pass_confirm" class="form-control <?php if(session('errors.pass_confirm')) : ?>is-invalid<?php endif ?>" placeholder="<?=lang('Auth.repeatPassword')?>" autocomplete="off">
-                        </div>
-
-                        <br>
-
-                        <button type="submit" class="btn btn-primary btn-block"><?=lang('Auth.register')?></button>
-                    </form>
-
-
-                    <hr>
-
-                    <p><?=lang('Auth.alreadyRegistered')?> <a href="<?= route_to('login') ?>"><?=lang('Auth.signIn')?></a></p>
+                    </div>
+                    <div class="mt-5 text-muted text-center">
+                        <p><?= lang('Auth.alreadyRegistered') ?> <a href="<?= route_to('login') ?>"><?= lang('Auth.signIn') ?></a></p>
+                    </div>
+                    <div class="simple-footer">
+                        Copyright &copy; SIMAD <?= date('Y'); ?>
+                    </div>
                 </div>
             </div>
-
         </div>
-    </div>
+    </section>
 </div>
 
-<?= $this->endSection() ?>
+<?= $this->endSection(); ?>
